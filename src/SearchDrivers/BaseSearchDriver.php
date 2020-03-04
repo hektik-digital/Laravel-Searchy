1fa0a459fbb2bfda84a62a325454d3c5665f7ea6
Edward Steven <simpleeddie@gmail.com>
2020-03-05 09:59:35 +1000

    Removed Line 123 for performance improvements
    We're not going to be using trashed items, need speed as this is on a search.

--- src/SearchDrivers/BaseSearchDriver.php
+++ src/SearchDrivers/BaseSearchDriver.php
@@ -120,8 +120,8 @@ abstract class BaseSearchDriver implements SearchDriverInterface
 
         // If they included withTrashed flag then give them all records including soft deletes
         // Check to ensure the column exists before committing
-        if( ! $this->withTrashed && in_array('deleted_at', Schema::getColumnListing($this->table)) )
-            $this->query = $this->query->where('deleted_at', NULL);
+        //if( ! $this->withTrashed && in_array('deleted_at', Schema::getColumnListing($this->table)) )
+        //    $this->query = $this->query->where('deleted_at', NULL);
 
         return $this->query
             ->orderBy($this->relevanceFieldName, 'desc')
